5b7f881520d8026b364d9f026daae411cc0a9d30
Stanley Louis <stanlouis@msn.com>
2017-03-25 12:08:22 -0700

    Component passing props

---
+++ app/index.js
@@ -0,0 +1,32 @@
+var React = require('react')
+var ReactDOM = require('react-dom');
+
+var FriendsContainer = React.createClass({
+  render: function(){
+    var name = 'Tyler McGinnis'
+    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
+    return (
+      <div>
+        <h3> Name: {name} </h3>
+        <ShowList names={friends} />
+      </div>
+    )
+  }
+});
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend, index){
+      return <li key={index}> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+ReactDOM.render(<FriendsContainer />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
