25611762b062fa7e9ee301832c3c9079536cbcb1
Stanley Louis <stanlouis@msn.com>
2017-03-25 17:11:20 -0700

    F.I.R.S.T Component nesting, and passing props

--- app/index.js
+++ app/index.js
@@ -1,32 +1,56 @@
 var React = require('react')
 var ReactDOM = require('react-dom');
 
-var FriendsContainer = React.createClass({
-  render: function(){
-    var name = 'Tyler McGinnis'
-    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
+var USER_DATA = {
+  name: 'Stanley Louis',
+  username: 'stanlouis',
+  image: 'https://avatars0.githubusercontent.com/u/7891366?v=3&s=460'
+}
+/*
+Components must be F.I.R.S.T
+Focus
+Independent
+Reusable
+Small
+Testable
+*/
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src= {this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function () {
     return (
       <div>
-        <h3> Name: {name} </h3>
-        <ShowList names={friends} />
+        <a href ={`https://www.github.com/${this.props.username}`}>
+          {this.props.username}
+        </a>
       </div>
     )
   }
 });
 
-var ShowList = React.createClass({
-  render: function(){
-    var listItems = this.props.names.map(function(friend, index){
-      return <li key={index}> {friend} </li>;
-    });
+var ProfileName = React.createClass({
+  render: function () {
     return (
       <div>
-        <h3> Friends </h3>
-        <ul>
-          {listItems}
-        </ul>
+        <h3>{this.props.name}</h3>
       </div>
     )
   }
 });
-ReactDOM.render(<FriendsContainer />, document.getElementById('app'));
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
